c709e19a4bfefda53176f2d38d7a95e1f7278ebd
Sonia Costa <niniccosta@gmail.com>
2017-07-08 00:45:34 +0100

    favicon64tentativa

---
+++ js/mapa.js
@@ -0,0 +1,49 @@
+/*
+ * declare map as a global variable
+ */
+var map;
+
+/*
+ * use google maps api built-in mechanism to attach dom events
+ */
+google.maps.event.addDomListener(window, "load", function () {
+
+  /*
+   * create map
+   */
+  var map = new google.maps.Map(document.getElementById("map_div"), {
+    center: new google.maps.LatLng(41.561043, -8.395860),
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  /*
+   * create infowindow (which will be used by markers)
+   */
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * marker creater function (acts as a closure for html parameter)
+   */
+  function createMarker(options, html) {
+    var marker = new google.maps.Marker(options);
+    if (html) {
+      google.maps.event.addListener(marker, "click", function () {
+        infoWindow.setContent(html);
+        infoWindow.open(options.map, this);
+      });
+    }
+    return marker;
+  }
+
+  /*
+   * add markers to map
+   */
+  var marker0 = createMarker({
+    position: new google.maps.LatLng(41.561043, -8.395860),
+    map: map,
+    icon: "https://swerc.eu/theme/images/faviconmiup.png"
+  }, "<h1>Marker 0</h1><p>This is the home marker.</p>");
+
+  
+});
